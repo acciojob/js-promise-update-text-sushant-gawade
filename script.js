cca961e6055d6b54ee1317de60ff7c95204b8afa
Sushant Gawade <78841663+sushant-gawade@users.noreply.github.com>
2025-11-05 12:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//your JS code here. If required.
-function hellowWorldPromise(){
-	return new Promise(resolve => {
-			setTimeout(() => resolve("Hello, World"), 1000);
-	});
+
+
+// Function returning a promise that resolves after 1 second
+function helloWorldPromise() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve("Hello, world!"), 1000);
+  });
 }
+
+// Use the promise to update the HTML element
 helloWorldPromise().then(message => {
-	document.getElementById("output").textContent = message;
+  document.getElementById("output").textContent = message;
 });
