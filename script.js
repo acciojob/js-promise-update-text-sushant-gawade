d4d22dbcaed3b98bf5be3042089a52e3947b562e
Sushant Gawade <78841663+sushant-gawade@users.noreply.github.com>
2025-11-05 12:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function hellowWorldPromise(){
+	return new Promise(resolve => {
+			setTimeout(() => resolve("Hello, World"), 1000);
+	});
+}
+helloWorldPromise().then(message => {
+	document.getElementById("output").textContent = message;
+});
